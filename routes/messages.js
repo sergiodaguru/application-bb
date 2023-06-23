9665b6a91886b708b8499c7177f62329fb92b98c
sergiod <sergiod@us.ibm.com>
2023-06-23 16:21:33 -0400

    Fifth commit, changed timings & filter message

--- routes/messages.js
+++ routes/messages.js
@@ -4,17 +4,17 @@ var router = express.Router();
 let ran = 0;
 function f() {
    ran++;
-   console.log("Test ABC Message " + ran)
-   setTimeout(f, 0);
+   console.log("Test ABC Message " + ran);
 }
 
 /* GET greeting */
 router.get('/messages', function(req, res, next) {
-  f();
+ setInterval(f,5000);
 
   setTimeout(() => { res.send(
                          { "Messages": ran }
-                       ) }, 5000);
+                       ) }, 2000);
+  clearInterval(f,15000);
 });
 
 module.exports = router;
